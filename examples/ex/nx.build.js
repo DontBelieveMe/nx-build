f2f0268e8b0a6307986a20b949456ca00e1c3ccb
Barney Wilks <barneywilks1@gmail.com>
2018-04-22 23:54:34 +0100

    Added note so I can remember to consider a different design style

--- examples/ex/nx.build.js
+++ examples/ex/nx.build.js
@@ -18,3 +18,41 @@ var fs = require('fs');
 fs.readFile(scriptDir + '/src/main.c', (err,data)=>{
     print(data.toString());
 });
+
+// tmp
+var nx = {};
+var platform = {};
+
+platform.isWin32  = () => { return true; };
+platform.isLinux = () => {return false;};
+
+nx.srcFiles = [
+    "src/main.c"
+];
+
+if(platform.isWin32()) {
+    nx.srcFiles.push('src/platformSpecific.win32.c');
+} else if(platform.isLinux()) {
+    nx.srcFiles.push('src/platformSpecific.win32.c');
+}
+
+nx.headerFiles = [
+    "include/main.h"
+];
+
+nx.targetName = "helloWorld";
+
+/*
+    Do we want member assignment style - e.g.
+        nx.targetName = ...;
+        nx.srcFiles = [ ... ];
+    
+    or member function style - e.g.
+        nx.addSrcFile("src/main.c");
+        nx.addSrcFiles([ ... ]);
+
+        nx.setTargetName("mathsLibrary");
+        nx.setTargetType("library");
+        
+    Maybe ask dev.to community for opinion?
+*/
