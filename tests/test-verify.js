21c991a78e61d3400c5fcb795c944b3cc8410d20
Barney Wilks <barneywilks1@gmail.com>
2018-04-23 22:19:14 +0100

    Added tests for verify.js module

---
+++ tests/test-verify.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var verify = require('../nxlib/verify');
+
+describe('verify', function(){
+    describe('#isOfType()', function() {
+        it('should throw an Error when the typeof the variable is not of the specified type', function(){
+            assert.throws(() => verify.isOfType(123, 'string'));
+        });
+
+        it('shoud throw an Error when the input object does not match the type of the required object', function(){
+            class ExpectedObjectType {
+            };
+
+            class ActualObjectType {
+            };
+
+            var test = new ActualObjectType();
+
+            assert.throws(() => verify.isOfType(test, ExpectedObjectType));
+        });
+    });
+});
